82deb6bd55158caccf258f3914b3d220be18191d
seonmin1 <https://github.com/seonmin1/ai_pintech_1st_p2.git>
2025-01-11 15:38:27 +0900

    design:상품페이지

--- src/main/java/org/koreait/front/controllers/ProductController.java
+++ src/main/java/org/koreait/front/controllers/ProductController.java
@@ -16,11 +16,9 @@ public class ProductController {
     public String product(Model model) {
 
         List<BaseModel> productNewsList = new ArrayList<>();
-        productNewsList.add(new BaseModel("해피햅삐 잠만보 이모티콘 출시!!", "2024년 07월 31일", "https://data1.pokemonkorea.co.kr/newdata/2024/07/2024-07-31_12-01-55-44458-1722394915.png", "https://e.kakao.com/t/happy-happyyy-jammanbo?lang=ko&referer=co_mkt"));
         model.addAttribute("productNewsList", productNewsList);
 
         List<Product>productGoodsList = new ArrayList<>();
-        productGoodsList.add(new Product("[성수미술관] 포켓몬 도안", "식품 및 기타", "https://data1.pokemonkorea.co.kr/newdata/2024/11/2024-11-21_13-47-51-31339-1732164471.jpg", "https://pokemonkorea.co.kr/goods/view/15455"));
         model.addAttribute("productGoodsList", productGoodsList);
 
         return "front/product/product";
--- src/main/java/org/koreait/front/repositories/ProductSwiperRepository.java
+++
@@ -1,7 +0,0 @@
-package org.koreait.front.repositories;
-
-import org.koreait.front.model.ProductSwiper;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductSwiperRepository extends JpaRepository<ProductSwiper, String> {
-}
